960f3bff45ac111341761faf539cbc18d4245ec8
T0ffik <t0fffik2000@gmail.com>
2024-08-03 11:49:02 +0200

    feat: add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      "border-defaul": "#CBB6E5",
+      "border-active": "#761BE4",
+      "border-error": "#ED4545",
+      "filed-default": "#FAF9FA",
+      "filed-error": "#FEECEC",
+      "cta-inactive": "#CBB6E5",
+      "cta-default": "#761BE4",
+      "cta-hover": "#6A19CD",
+      "text-primary": "#000853",
+      "text-secondary": "#FFFFFF",
+      "icon-default": "#000853",
+      "icon-hover": "#ED4545",
+    },
+    fontSize: {
+      extraBig: "24px",
+      big: "18px",
+      medium: "16px",
+      small: "14px",
+      ExtraSmall: "12px",
+    },
+  },
+  plugins: [],
+};
