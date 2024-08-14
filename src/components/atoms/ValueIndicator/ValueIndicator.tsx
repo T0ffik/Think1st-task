48f0a5f2aab431c2c6761c2dd1a849e7c479dbf7
T0ffik <t0fffik2000@gmail.com>
2024-08-14 12:58:53 +0200

    get rid off console.logs

--- src/components/atoms/ValueIndicator/ValueIndicator.tsx
+++ src/components/atoms/ValueIndicator/ValueIndicator.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { MIN_VALUE } from "../RangeInput";
 import { positions, positionsMobile } from "./positions";
 
@@ -7,20 +6,6 @@ type ValueIndicatorProps = {
 };
 
 export const ValueIndicator = ({ value }: ValueIndicatorProps) => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const setPosition = () => {
-    console.log(windowWidth);
-    if (windowWidth < 475) {
-      return;
-    }
-  };
-  console.log(window.innerWidth);
-  console.log(windowWidth);
-  setPosition();
-  useEffect(() => {
-    console.log(1);
-    setWindowWidth(window.innerWidth);
-  }, [window.innerWidth]);
   return (
     <div
       className={`bg-[url('/indicator.png')] w-[37px] h-[31px] text-center pt-[6px] absolute text-cBorder-active ${
